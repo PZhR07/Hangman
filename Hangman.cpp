8b9fa690bf29c56349b0d2ec17f9554f926fa813
parsa <132606568+PZhR07@users.noreply.github.com>
2026-06-18 21:11:44 +0330

    Update Hangman.cpp

--- Hangman.cpp
+++ Hangman.cpp
@@ -2,28 +2,57 @@
 #include <string>
 #include <cstdlib>
 #include <ctime>
+#include <fstream>
 
 using namespace std;
 
 class hangman {
-private:
-    char x , l ,y;
-    string w;
-    string guessed;
-    string correct_word;
-    int attempt_counter = 6;
-    
-public:
-    string random_word();
-    bool result(char l);
-    void play();
-    char* split(string correct_word);
-    int level();
-    void random_hint();
-    void hint();
+    private:
+        char x , l ,y;
+        string w;
+        string guessed;
+        string correct_word;
+        int attempt_counter = 6;
 
+    public:
+        string random_word();
+        bool result(char l);
+        void play();
+        char* split(string correct_word);
+        int level();
+        void random_hint();
+        void hint();
+        void Score_System(int score);
+        void saveScore(int score);
+        int loadScore();
+        void game_history();
+        static int score;
 };
+int hangman::score = 0;
 
+void hangman::game_history(){
+    // placeholder for future implementation
+}
+void hangman::saveScore(int score){
+    ofstream file("score.txt");
+    file << score;
+    file.close();
+}
+int hangman::loadScore(){
+    ifstream file("score.txt");
+    int s = 0;
+    if (file.is_open()){
+        file >> s;
+    }
+    return s;
+}
+void hangman::Score_System(int score){
+    cout<<"your score is:"<<score<<endl;
+    if (score >=20){
+        random_hint();
+        hangman::score -= 20;
+    }
+}
 void hangman::random_hint(){
     int random_hints = rand()%3;
     switch(random_hints){
@@ -74,24 +103,19 @@ string hangman::random_word(){
 }
 char* hangman::split(string correct_word){
     char* arr = new char[correct_word.length() + 1];
-
-    for(int i=0; i<correct_word.length(); i++){
+    for(int i=0; i<(int)correct_word.length(); i++){
         arr[i] = correct_word[i];
     }
-
     arr[correct_word.length()] = '\0';
-
     return arr;
 }
 bool hangman::result(char l){
-
     if (guessed.find(l) == string::npos){
         guessed += l;
     }
 
     bool win = true;
-
-    for (int i = 0; i < correct_word.length(); i++){
+    for (int i = 0; i < (int)correct_word.length(); i++){
         if (guessed.find(correct_word[i]) == string::npos){
             win = false;
             break;
@@ -100,23 +124,27 @@ bool hangman::result(char l){
 
     if (win){
         cout << "That's right(" << correct_word << ")! you win" << endl;
+        score +=5;
         return true;
     }
 
     return false;
 }
 void hangman::play(){
+    cout<<"if you win you get 5 points and if you lose you get 0 points"<<endl;
+    cout<<"if you win 20 points you can get a hint in the next game"<<endl;
     attempt_counter = level();
     correct_word = random_word();
     guessed = "";
 
-    for(int i=0; i<correct_word.length(); i++){
+    // نمایش کلمه با _ در ابتدا
+    for(int i=0; i<(int)correct_word.length(); i++){
         cout << "_ ";
     }
     cout << endl;
 
     for (int fault_counter=0 ; fault_counter<attempt_counter;){
-        if (fault_counter == attempt_counter/2){
+        if (fault_counter == attempt_counter/2 && fault_counter > 0){
             hint();
         }
         cout << "Do you want to guess a word(y/n):";
@@ -128,6 +156,7 @@ void hangman::play(){
 
             if (w == correct_word){
                 cout << "That's right! you win" << endl;
+                score +=5;
                 break;
             } else {
                 fault_counter++;
@@ -142,7 +171,7 @@ void hangman::play(){
             char* arr = split(correct_word);
             bool found = false;
 
-            for (int i = 0; i < correct_word.length(); i++){
+            for (int i = 0; i < (int)correct_word.length(); i++){
                 if (arr[i] == l){
                     cout << l << " found in position " << i + 1 << endl;
                     found = true;
@@ -161,22 +190,35 @@ void hangman::play(){
                 cout << "You lose! The correct word was: " << correct_word << endl;
                 break;
             }
+
+            // نمایش پیشرفت کلمه بعد از هر حدس
+            for(int i=0; i<(int)correct_word.length(); i++){
+                if(guessed.find(correct_word[i]) != string::npos)
+                    cout << correct_word[i] << " ";
+                else
+                    cout << "_ ";
+            }
+            cout << endl;
         }
     }
+
+    // ذخیره score بعد از هر بازی
+    saveScore(score);
+    Score_System(score);
 }
 
 int main(){
+    srand(time(0));
+    hangman game;
+    hangman::score = game.loadScore();
+
+    char again;
     do {
-        hangman game;
         game.play();
-        
-        cout << "Play again? (y/n): ";
-        char c;
-        cin >> c;
+        cout << "Do you want to play again? (y/n): ";
+        cin >> again;
+    } while(again == 'y');
 
-        if (c != 'y') break;
-
-    } while (true);
-
+    game.saveScore(hangman::score);
     return 0;
 }
